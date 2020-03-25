536d22c06fcf9543dd68890140bf5e2e2811da56
Diego Kaleb Valenzuela Carrillo <d.kal3b@gmail.com>
2020-03-24 19:21:38 -0700

    Placing Building System
    - Camera controller
    - Building prefabs
    - All assets
    - Placing buildings with mouse click

---
+++ City Builder/Assets/Scripts/BuildingHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHandler : MonoBehaviour
+{
+    public BuildableObject[] buildables;
+    public Camera mainCamera;
+    public LayerMask buildableAreaMask;
+
+    [SerializeField]
+    private int selectionIndex;
+
+    public void Select(int index)
+    {
+        if(index >= 0 || index < buildables.Length)
+        {
+            selectionIndex = index;
+        }
+    }
+
+    public void Deselect()
+    {
+        selectionIndex = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectionIndex >= 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                Physics.Raycast(ray, out hit, Mathf.Infinity, buildableAreaMask);
+
+                if (hit.collider != null)
+                {
+                    BuildableObject buildable = buildables[selectionIndex];
+                    Instantiate(buildable.prefab, hit.point + buildable.offset, Quaternion.identity);
+                }
+            }
+        }
+    }
+}
